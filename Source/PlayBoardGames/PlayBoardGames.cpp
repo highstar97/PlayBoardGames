3dc83508ca3e43912cb8554205fdea67bbe44be5
highstar97 <pgt0185@gmail.com>
2024-04-22 17:09:58 +0900

    [Add] Add Unreal Project

---
+++ Source/PlayBoardGames/PlayBoardGames.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PlayBoardGames.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PlayBoardGames, "PlayBoardGames" );
