b235bcb4e45e6e15f7cbf45cb7e3e82ba49527a5
highstar97 <pgt0185@gmail.com>
2024-06-11 23:34:28 +0900

    [Add] Gameplay Message Subsystem Plugin 추가
    * Gameplay Message Subsystem Plugin 추가

--- Source/PlayBoardGames/PlayBoardGames.Build.cs
+++ Source/PlayBoardGames/PlayBoardGames.Build.cs
@@ -8,7 +8,7 @@ public class PlayBoardGames : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam", "GameplayMessageRuntime" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
